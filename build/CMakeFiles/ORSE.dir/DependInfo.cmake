d5b9026eb80404e2203b5fcac2b627a7f42d637d
Dima-Meln <dimamelnichuk11@gmail.com>
2012-06-05 08:45:25 +0300

    Signed-off-by: Dima-Meln <dimamelnichuk11@gmail.com>

---
+++ build/CMakeFiles/ORSE.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dima/projects/ORSE/src/entity.cpp" "/home/dima/projects/ORSE/build/CMakeFiles/ORSE.dir/src/entity.cpp.o"
+  "/home/dima/projects/ORSE/src/network/auth.cpp" "/home/dima/projects/ORSE/build/CMakeFiles/ORSE.dir/src/network/auth.cpp.o"
+  "/home/dima/projects/ORSE/src/network/connection.cpp" "/home/dima/projects/ORSE/build/CMakeFiles/ORSE.dir/src/network/connection.cpp.o"
+  "/home/dima/projects/ORSE/src/node.cpp" "/home/dima/projects/ORSE/build/CMakeFiles/ORSE.dir/src/node.cpp.o"
+  "/home/dima/projects/ORSE/src/resourcemgr.cpp" "/home/dima/projects/ORSE/build/CMakeFiles/ORSE.dir/src/resourcemgr.cpp.o"
+  "/home/dima/projects/ORSE/src/root.cpp" "/home/dima/projects/ORSE/build/CMakeFiles/ORSE.dir/src/root.cpp.o"
+  "/home/dima/projects/ORSE/src/scenemanager.cpp" "/home/dima/projects/ORSE/build/CMakeFiles/ORSE.dir/src/scenemanager.cpp.o"
+  "/home/dima/projects/ORSE/src/viewport.cpp" "/home/dima/projects/ORSE/build/CMakeFiles/ORSE.dir/src/viewport.cpp.o"
+  "/home/dima/projects/ORSE/src/window.cpp" "/home/dima/projects/ORSE/build/CMakeFiles/ORSE.dir/src/window.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/ORSE.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ORSE.dir/src/root.cpp.o"
+  "CMakeFiles/ORSE.dir/src/resourcemgr.cpp.o"
+  "CMakeFiles/ORSE.dir/src/viewport.cpp.o"
+  "CMakeFiles/ORSE.dir/src/scenemanager.cpp.o"
+  "CMakeFiles/ORSE.dir/src/window.cpp.o"
+  "CMakeFiles/ORSE.dir/src/network/connection.cpp.o"
+  "CMakeFiles/ORSE.dir/src/network/auth.cpp.o"
+  "CMakeFiles/ORSE.dir/src/node.cpp.o"
+  "CMakeFiles/ORSE.dir/src/entity.cpp.o"
+  "libORSE.pdb"
+  "libORSE.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ORSE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
